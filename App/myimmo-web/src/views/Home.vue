fbcf41a1fc2443d8a47aff310cb8c66eaf96afcc
Natane <natane-b@hotmail.fr>
2021-05-06 15:07:47 +0200

    Initialisation de l'application web

---
+++ App/myimmo-web/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <InputAdresse />
+</template>
+
+<script>
+  import InputAdresse from '../components/InputAdresse.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      InputAdresse,
+    },
+  }
+</script>
